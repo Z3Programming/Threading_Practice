76b635f064bddd88058879b396d5e5228ab1b024
Z3Programming <z3programming@gmail.com>
2018-12-17 18:43:52 -0500

    Changed namespace name

--- Thread/Thread/Program.cs
+++ Thread/Thread/Program.cs
@@ -2,14 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace Thread
+
+namespace Thread_Practice
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Thread t = new Thread(Print1);
+
+            t.Start();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(0);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Print1()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(1);
+            }
         }
     }
 }
